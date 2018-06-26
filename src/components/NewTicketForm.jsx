1a9a6240c60216eb46bc5d624903354318eeb014
Sean McDermott <seanjmac@hotmail.com>
2018-06-26 16:45:18 -0700

    add styling to new ticket component

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -3,6 +3,32 @@ import React from 'react';
 function NewTicketForm() {
   return(
     <div>
+      <style jsx>{`
+      form {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        width: 30%;
+        margin: 10px auto;
+      }
+      input {
+        margin-bottom: 10px;
+        border-radius: 5px;
+        height: 30px;
+        background-color: black;
+        opacity: 0.5;
+      }
+      textarea {
+        height: 10vw;
+        background-color: black;
+        opacity: 0.5;
+      }
+      button {
+        width: 50%;
+        margin: 10px auto;
+        background-color: #5788d1;
+      }
+      `}</style>
       <form>
         <input
           type='text'
