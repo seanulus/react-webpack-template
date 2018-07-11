61790ecb68dd937b9b8c6da917b1fc50ec3a5bcf
Cameron Button and Sean McDermott <cameron@>
2018-07-11 13:40:23 -0700

    Add tests for wait times and implement coding for formatted wait times

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,11 +22,17 @@ class App extends React.Component {
   }
 
   updateTicketElapsedWaitTime() {
-    // let newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-    // Object.keys(newMasterTicketList).forEach(ticketId => {
-    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-    // });
-    // this.setState({masterTicketList: newMasterTicketList});
+    const { dispatch } = this.props;
+    Object.keys(this.props.masterTicketList).map(ticketId => {
+      const ticket = this.props.masterTicketList[ticketId];
+      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const action = {
+        type: 'UPDATE_TIME',
+        id: ticketId,
+        formattedWaitTime: newFormattedWaitTime
+      };
+      dispatch(action);
+    });
   }
 
   render() {
